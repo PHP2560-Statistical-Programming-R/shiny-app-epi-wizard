0b2df5760fe5bc5fd6a89cd648d7becc1f855ee1
Ze Zhang <ze_zhang@brown.edu>
2017-12-15 23:45:00 -0500

    Ze pvalue calculator

--- Individual/Ze_app_pvalue.R
+++ Individual/Ze_app_pvalue.R
@@ -1,16 +1,27 @@
 library(shiny)
 
-ui <- fluidPage(
-  h1("the probability of achieving z value in a standard normal distribution"),
-  wellPanel(sliderInput(inputId = "num", 
-              label = "Z value", 
-              value = 1, step = 0.01, min = 0, max = 2)),
-  textOutput("pvalue")
+ui <- fluidPage(h1("the probability of achieving z value or greater in a standard normal distribution"),
+       #Using siderbar layout for data input         
+                sidebarLayout(
+                  sidebarPanel(
+                    textInput("num",
+                              "z-value:",
+                              value = ""),
+                    submitButton(text = "Calculate p-value")
+                  ),
+                  mainPanel(
+                    textOutput("pvalue")
+                    
+                  )
+                )
 )
+
+
 
 server <- function(input, output) {
   output$pvalue <- renderText({ cbind("p-value",
-    round(pnorm(input$num, mean = 0, sd=1, lower.tail = FALSE),3))
+    #p-value calculation round up to 3
+  round(pnorm(as.numeric(input$num), mean = 0, sd=1, lower.tail = FALSE),3))
   })
 }
 
